e3820c4d906076b001625bc7cc6bb98b409746ab
Ostronkex <erikmagnusson.privat@gmail.com>
2024-07-27 12:23:16 +0200

    Remove day2 files

--- day2-lesson5-data-types.py
+++
@@ -1,9 +0,0 @@
-two_digit_number = input()
-# 🚨 Don't change the code above 👆
-####################################
-# Write your code below this line 👇
-
-numOne = int(two_digit_number[0])
-numTwo = int(two_digit_number[1])
-
-print(numOne + numTwo)
--- day2-lesson7-life-in-weeks.py
+++
@@ -1,9 +0,0 @@
-age = input()
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-
-age = int(age)
-
-weeksLeft = (90 - age) * 52
-
-print(f"You have {weeksLeft} weeks left.")
