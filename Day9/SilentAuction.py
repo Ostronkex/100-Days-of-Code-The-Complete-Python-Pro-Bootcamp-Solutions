43471dd676cefb0ff35aeccd5bb02db71470dbbf
Erik Magnusson <erikmagnusson.privat@gmail.com>
2024-08-16 14:28:10 +0200

    Update SilentAuction.py
    Initial code for the silent auction. This is a placeholder, nothing works as expected at the moment.
    
    Follow this flowchart
    https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Blind%20Auction%20Flow%20Chart#R3VnbcpswEP0aPzYDCLB5tHNrZ9pMpu6kzaMMilEDiBHyLV%2FfFYirHMdp7JD4JUGrXV3Ont2V5AE6j9fXHKfhDxaQaGAZwXqALgaWZRqmAf%2BkZFNIHM8qBHNOA6VUC6b0iZSWSrqgAclaioKxSNC0LfRZkhBftGSYc7Zqqz2wqD1riudEE0x9HOnS3zQQoZK6jl13fCV0HpZTm65X9MS41FZbyUIcsFVDhC4H6JwzJoqveH1OIoleCUxhd%2FVMb7UyThKxj8Hs4R5fx%2BMlu%2F52dxPepdM7H31BxShLHC3UjtVixaaEgASAiGoyLkI2ZwmOLmvphLNFEhA5jQGtWuc7YykITRD%2BJUJslHvxQjAQhSKOVG8xp5zo2b0pUcYW3Cc7NlSSBPM5ETv0rMoDwF3CYiL4Buw4ibCgy%2FY6sCLRvNKrYYYPhfQrUDc11Ke%2Fxj9%2FadDXwEqUViEVZJrifP8riLdtIC4JF2S9G0Z928rAHimuqmi1VXNVU99TorBBemQcCSfn1Nhp7clOu092Wjo7Q0hclhEBcDKJchbn6VWcpZsPx1mvZ84OT42z9p6cdfvkrK2hPs4e84LP4e8NjokcIUkXon%2FCOk6LsKajM9a0t1DWPhZlvVOjrLsnZUd9UtbdSdkJ2FrGLaew0Y%2FG2OpI2xtjTf0E9ckp%2B1YqKtNbRmHmynOO1fYcGnU8UoSIsuo4pVrG%2F%2FtppHM8COqEjJOgojqsQB4usLwsUl9QcESOAKwUIjmU6o9kUxnlQ54NLBfHkvDJLEsrh%2FQZKmh41gmWLQcSc%2BjoweIcLVhGfcQGWVPxR5nL7%2FuG%2FGLd6LjYlI0EdpubAISqed%2Fsq83yVml3wCg00Z6VwxweIl7HnONNQyGVcZg9H872yGxxy%2B5e%2BTv6yHN26cNHsYKDxnwFdi90Mxt0Uzw6CcI1noz6eLDQ34mKwwp9KLIzz9N5%2FpfJ5kCu141gR5MZNNy5%2FFpkhMtsvgplpl%2FhnKp51p%2FJEtBN3SGLZ4usp7RdVcqX0na3oh4Oc%2F2AeE8yDSbYtGjjgSM6T%2BDbh82DJ9BEQkN9HI1VR0yDoIg6ktEnPMuHksRW%2BQfGdSYD50KOBYGWFTF3IKiRZ7dzkmFrQG87S1pHw9nrtTxWOeo1BbKVrerk9Q75arhnvnrrC9b2gmZ1HnvQcL%2Fz7GsLbbdwOvbuQttd12v1zeE7FGZTf5o6jwjm1fk68zkhiZ6I3%2FkMjZzOK%2FSWVLztSe9ot80y9TSAu6L5LaTALYQ1kEyUdQyuMlVX%2ByqT2wTEj4pKCSpx%2B4azokkii2fPHnCGL3vA9LYUQ%2FdoLrA0F9ywT18LXdTB2dBxRoephdCsf2osMkr9iy26%2FAc%3D#%7B%22pageId%22%3A%22wMYETGOgq5MebSD-sj3f%22%7D

--- Day9/SilentAuction.py
+++ Day9/SilentAuction.py
@@ -1,1 +1,67 @@
-# Start of silent auction project for day 9
+# Start of silent auction
+
+gavel = """                        
+    ___________
+    \\         /
+     )_______(
+     |       |_.-._,.---------.,_.-._
+     |       | | |               | | ''-.
+     |       |_| |_             _| |_..-'
+     |_______| '-' `'---------'` '-'
+     )       (
+    /_________\\
+    `'-------'`
+  .-------------.
+ /_______________\\
+"""
+
+name = ""
+bid = 0
+numbers = [0,1,2,3,4,5,6,7,8,9]
+
+def main():
+    global gavel
+    print(gavel)
+    print("Welcome to the silent auction!")
+    setup()
+
+def setup():
+    global name, bid, numbers
+    bidder = {
+        "name": "",
+        "bid": None
+    }
+    def my_name():
+        bidder["name"] = input("What is your name? ")
+        if isinstance(bidder["name"], str):
+            pass
+        else:
+            print("Only letters allowed")
+            my_name()
+    def my_bid():
+        bidder["bid"] = input("What is your bid? ")
+        if bidder["bid"].isdigit():
+            bidder["bid"] = int(bidder["bid"])
+        else:
+            print("Only numbers are allowed")
+            my_bid()
+main()
+
+# Archived code for now, might be useful later
+""" if bidder["bid"] in numbers:
+    bidder["bid"] = int(bidder["bid"])
+    pass
+else:
+    print("Only numbers are allowed")
+    setup()
+    print(f"You bid {bidder["bid"]}")
+    bidder["name"] = input("What is your name? ")
+    if isinstance(bidder[name], str):
+        pass
+    else:
+        print("Only letters are allowed, start over")
+        setup()
+    print(bidder["name"])
+
+setup() """
+
